61cf71a1ceed28fcd6b88f3b4fd1d08fc8b4f693
Ankush Singhal <ankushsinghal1995@gmail.com>
2019-05-19 16:08:14 +0530

    GTest Basic test creation
    1) Created Basic Test case using GTest
    2) Created the CMakeLists.txt file to run the Test
    3) Added README for how to run the Tests

---
+++ Arrays/README.md
@@ -0,0 +1,5 @@
+# Steps to run Resizable Array Tests
+
+1. cmake CMakeLists.txt
+2. make
+3. ./resizableArrayTest
---
+++ Arrays/resizable_array_test.cc
@@ -0,0 +1,19 @@
+/**
+ *  @author       : Ankush Singhal [ankushsinghal1995@gmail.com]
+ *  @date         : Sunday, 19th May, 2019
+ *  @description  : Source file for Resizable Array Class Unit Testitng using GTest 
+ *  @file         : resizable_array_test.cpp
+ */
+
+#include "resizable_array.h"
+#include <gtest/gtest.h>
+
+TEST(ResizableArrayTest, InitializationTest){
+    ResizableArray resizable_array;
+    ASSERT_EQ(resizable_array.size(), 0);
+}
+
+int main(int argc, char **argv){
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
